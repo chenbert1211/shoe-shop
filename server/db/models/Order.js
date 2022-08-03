bce5011752e20d6c99ba5b31e1b35a7e9f42cd21
Watson Chen <wc.watsonchen@gmail.com>
2022-08-03 15:09:40 -0400

    3PM merge

--- server/db/models/Order.js
+++ server/db/models/Order.js
@@ -1,9 +1,9 @@
-const Sequelize = require('sequelize');
-const db = require("../db")
+const Sequelize = require("sequelize");
+const db = require("../db");
 
-const Order = db.define('order', {
-    status: {
-        type: Sequelize.ENUM ("open","closed"),
-    }
-})
+const Order = db.define("order", {
+  status: {
+    type: Sequelize.ENUM("open", "closed"),
+  },
+});
 module.exports = Order;
