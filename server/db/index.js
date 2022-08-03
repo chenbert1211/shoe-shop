5c9eed5eeda1d3882c1716321b167be836ef2cda
Watson Chen <wc.watsonchen@gmail.com>
2022-08-03 12:57:40 -0400

    created Order model

--- server/db/index.js
+++ server/db/index.js
@@ -1,18 +1,23 @@
 //this is the access point for all things database related!
 
-const db = require('./db')
+const db = require("./db");
 
-const User = require('./models/User')
-const Product = require("./models/Product")
+const User = require("./models/User");
+const Product = require("./models/Product");
+const Order = require("./models/Order");
 
 //associations could go here!
 
+Order.belongsTo(User);
+User.hasMany(Order);
 
+Product.belongsToMany(Order, { through: Order_Product });
+Order.belongsToMany(Product, { through: Order_Product });
 
 module.exports = {
   db,
-  models:{
+  models: {
     User,
-    Product
-  }
-}
+    Product,
+  },
+};
