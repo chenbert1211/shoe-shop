0ec30ec4465dc8029132c3f4fb8f6d9b465ffede
emrebsbydr <emrebasbaydar@gmail.com>
2022-08-04 12:50:15 -0400

    order component routes , delete method added

--- server/api/order.js
+++ server/api/order.js
@@ -1,7 +1,7 @@
 const router = require("express").Router();
 const e = require("express");
 const {
-  models: { Order,Product },
+  models: { Order,Product,Order_Product },
 } = require("../db");
 
 //localhost/order/
@@ -15,32 +15,51 @@ router.get("/", async (req, res, next) => {
   }
 });
 
-//
+//localhost/order/:userId
+router.get("/", async (req, res, next) => {
+  const id = req.params.userId;
+  try {
+    const singleOrder = await Order.findone({
+      where: {
+        id:id
+      }
+    });
+    res.json(singleOrder);
+  } catch (err) {
+    next(err);
+  }
+});
 
 //POST
 //localhost/order/:userId
 router.post("/:userId", async(req,res,next) =>{
   try {
-    console.log("id", req.params.id)
-    const newOrder = await Order.create(req.params)
+    // console.log("id", req.params.id)
+    const newOrder = await Order.create(req.body)
     res.status(201).send(newOrder)
   } catch (err) {
     next(err);
   }
 })
 
-
-module.exports = router;
-
-/*
-router.post("/", async(req, res, next) => {
+//DELETE
+//localhost/order/delete/:orderId
+router.delete("/delete/:orderId", async(req, res, next) => {
   try {
-    // console.log("req.body campuses create:", req.body)
-    const newCampus = await Campus.create(req.body)
-    res.status(201).send(newCampus)
+    let id= req.params.id
+    const  order = await Order.findByPk(id)
+    await order.destroy()
+    res.send(order)
   } catch (error) {
     next(error)
   }
 })
 
-*/
+
+
+
+
+
+module.exports = router;
+
+
