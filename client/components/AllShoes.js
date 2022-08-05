139313a5078f2b3ce466a8e687ba5244350e760d
emrebsbydr <emrebasbaydar@gmail.com>
2022-08-05 15:53:26 -0400

    initial local

--- client/components/AllShoes.js
+++ client/components/AllShoes.js
@@ -1,5 +1,43 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { fetchShoes } from '../store/redux/allShoes';
 
-export default function AllShoes() {
-  return <div>AllShoes</div>;
+export class AllShoes extends Component {
+  componentDidMount() {
+    this.props.getShoes();
+  }
+
+  render() {
+    const allShoes = this.props.allShoes;
+    return (
+      <div id="all-shoes-view">
+        <h1>All Shoes</h1>
+        <div id="product-view">
+          {allShoes.map((shoe) => {
+            return (
+              <Link key={shoe.id} to={`/product/${shoe.id}`}>
+                <div className="product">
+                  <img className="product-image" src={shoe.imageUrl} />
+                  <div>{shoe.name}</div>
+                </div>
+              </Link>
+            );
+          })}
+        </div>
+      </div>
+    );
+  }
 }
+
+const mapStateToProps = (reduxState) => {
+  return {
+    allShoes: reduxState.allShoesReducer,
+  };
+};
+
+const mapDispatchToProps = (dispatch) => ({
+  getShoes: () => dispatch(fetchShoes()),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(AllShoes);
--- client/components/Navbar.js
+++ client/components/Navbar.js
@@ -9,7 +9,10 @@ const Navbar = ({ handleClick, isLoggedIn }) => (
       <div id="navbar">
         <input id="searchbar" type="text" placeholder="Search"></input>
         {/* <h1 id="logo-text">FlightClub 2: Still Flying</h1> */}
-        <img id="nbf-logo" src="/nbf-logo-3.png" />
+        <Link to="/">
+          <img id="nbf-logo" src="/nbf-logo-3.png" />
+        </Link>
+
         <div id="left-nav-buttons">
           <div id="user-container">
             <a>Guest</a>
