6c256f0af4e305b34561ed3e2b207a8ef85330d4
Cole Berman <11coleandrew4@gmail.com>
2022-08-08 17:19:11 -0500

    new prospective main branch

--- client/components/AllShoes.js
+++ client/components/AllShoes.js
@@ -1,55 +1,3 @@
-// import React, { Component } from 'react';
-// import { Link } from 'react-router-dom';
-// import { connect } from 'react-redux';
-// import { fetchShoes } from '../store/redux/allShoes';
-// import { getUserCart } from '../store/redux/cart';
-
-// export class AllShoes extends Component {
-//   componentDidMount() {
-//     this.props.getShoes();
-//     if(!!this.props.auth.id)
-//     {
-//       this.props.getUserCart(this.props.auth.id)
-//     }
-//   }
-
-//   render() {
-//     const allShoes = this.props.allShoes;
-//     // console.log(this.props)
-//     return (
-//       <div id="all-shoes-view">
-//         <h1>All Shoes</h1>
-//         <div id="product-view">
-//           {allShoes.map((shoe) => {
-//             return (
-//               <Link key={shoe.id} to={`/product/${shoe.id}`}>
-//                 <div className="product">
-//                   <img className="product-image" src={shoe.imageUrl} />
-//                   <div>{shoe.name}</div>
-//                 </div>
-//               </Link>
-//             );
-//           })}
-//         </div>
-//       </div>
-//     );
-//   }
-// }
-
-// const mapStateToProps = (reduxState) => {
-//   return {
-//     allShoes: reduxState.allShoesReducer,
-//     auth: reduxState.auth
-//   };
-// };
-
-// const mapDispatchToProps = (dispatch) => ({
-//   getShoes: () => dispatch(fetchShoes()),
-//   getUserCart: (id) => dispatch(getUserCart(id))
-// });
-
-// export default connect(mapStateToProps, mapDispatchToProps)(AllShoes);
-
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
@@ -66,6 +14,7 @@ export class AllShoes extends Component {
   componentDidMount() {
     this.props.getShoes();
   }
+
   clickHandler(event) {
     const target = event.target.innerHTML;
     this.setState({
@@ -73,13 +22,12 @@ export class AllShoes extends Component {
     });
     console.log('state', this.state.brand);
     console.log('target value', target);
-
   }
 
   render() {
     const allShoes = this.props.allShoes;
     console.log(allShoes);
-    const {clickHandler} = this;
+    const { clickHandler } = this;
     //  const filteredArr = allShoes.filter((shoe) => {
     //   if (this.state.brand === 'all') {
     //     return shoe
