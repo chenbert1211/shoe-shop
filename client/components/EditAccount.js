9076ba5b366f820821bdc16c2705f2ec914c4afa
Watson Chen <wc.watsonchen@gmail.com>
2022-08-08 15:30:32 -0400

    designed the edit account page

--- client/components/EditAccount.js
+++ client/components/EditAccount.js
@@ -1,98 +1,159 @@
 import React from "react";
-import axios from 'axios';
-import { connect } from 'react-redux';
-import { Link } from 'react-router-dom';
-import { updateUser } from '../store/auth';
+import axios from "axios";
+import { connect } from "react-redux";
+import { Link } from "react-router-dom";
+import { updateUser } from "../store/auth";
 
-class EditAccount extends React.Component{
-constructor(props){
-    super(props)
-     this.state = {
-        id: this.props.auth.id,
-        firstName: null,
-        lastName: null,
-        imageUrl: null,
-        phoneNumber: null,
-        email: null
-        }
-        
-        this.handleChange = this.handleChange.bind(this)
-        this.handleSubmit = this.handleSubmit.bind(this)
-}
+class EditAccount extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      id: this.props.auth.id,
+      firstName: null,
+      lastName: null,
+      imageUrl: null,
+      phoneNumber: null,
+      email: null,
+    };
 
-handleChange(event){
-     this.setState({
-       [event.target.name]: event.target.value
-     })
-   }
-   
-   handleSubmit(event){
-     event.preventDefault();
-     this.props.updateUser({...this.state})
-   }
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
 
-componentDidMount(){
-    const { auth } = this.props
+  handleChange(event) {
     this.setState({
-        firstName: auth.firstName,
-        lastName: auth.lastName,
-        imageUrl: auth.imageUrl,
-         phoneNumber: auth.phoneNumber,
-        email: auth.email
-    })
-}
+      [event.target.name]: event.target.value,
+    });
+  }
 
-render()
-{
-    const { firstName, lastName, imageUrl, phoneNumber, email } = this.state
+  handleSubmit(event) {
+    event.preventDefault();
+    this.props.updateUser({ ...this.state });
+  }
+
+  componentDidMount() {
+    const { auth } = this.props;
+    this.setState({
+      firstName: auth.firstName,
+      lastName: auth.lastName,
+      imageUrl: auth.imageUrl,
+      phoneNumber: auth.phoneNumber,
+      email: auth.email,
+    });
+  }
+
+  render() {
+    const { firstName, lastName, imageUrl, phoneNumber, email } = this.state;
     // console.log(this.state)
-    return(
-        <div className='Account'>
-        <form onSubmit={this.handleSubmit}>
-        
-        <br></br><br></br><br></br><br></br>
-        
-        <h2>Account Information</h2>
-            <label>
-            First Name
-        <input placeholder="First Name"  onChange={this.handleChange} className='input' name="firstName" value={firstName}/>
-            </label>
-    <br></br>
-            <label>
-            Last Name
-        <input placeholder="Last Name" onChange={this.handleChange} className='input' name="lastName" value={lastName}/>
-            </label><br></br>
-            <label>
-            Profile Picture
-        <input placeholder="Profile Picture" onChange={this.handleChange} className='input' name="imageUrl" value={imageUrl}/>
-            </label><br></br>
-            <label>
-            Cell Number
-        <input placeholder="Cell Number" onChange={this.handleChange} className='input' name="phoneNumber" value={phoneNumber}/>
-            </label><br></br>
-            <label>
-            Email
-        <input placeholder="Email" onChange={this.handleChange} className='input' name="email" value={email}/>
-            </label><br></br>
-    <input type="submit"/>
-  <Link to='/account'><button >Cancel</button></Link>
-</form>
-  </div>
-        )
-}
+    return (
+      <div id="edit_account_body">
+        <div class="edit_account_content">
+          <form id="edit_account" onSubmit={this.handleSubmit}>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+
+            <div className="edit_account">
+              <h1>Account Information</h1>
+              <div class="input-box">
+                <label className="details" for="first_name">
+                  First Name
+                  <input
+                    placeholder="First Name"
+                    onChange={this.handleChange}
+                    className="input"
+                    name="firstName"
+                    value={firstName}
+                  />
+                </label>
+              </div>
+              <br></br>
+
+              <div class="input-box">
+                <label className="details" for="last_name">
+                  Last Name
+                  <input
+                    placeholder="Last Name"
+                    onChange={this.handleChange}
+                    className="input"
+                    name="lastName"
+                    value={lastName}
+                  />
+                </label>
+              </div>
+
+              <br></br>
+
+              <div class="input-box">
+                <label className="details" for="profile_picture">
+                  Profile Picture
+                  <input
+                    placeholder="Profile Picture"
+                    onChange={this.handleChange}
+                    className="input"
+                    name="imageUrl"
+                    value={imageUrl}
+                  />
+                </label>
+              </div>
+
+              <br></br>
+
+              <div class="input-box">
+                <label className="details" for="cell_number">
+                  Cell Number
+                  <input
+                    placeholder="Cell Number"
+                    onChange={this.handleChange}
+                    className="input"
+                    name="phoneNumber"
+                    value={phoneNumber}
+                  />
+                </label>
+              </div>
+              <br></br>
+
+              <div class="input-box">
+                <label className="details" for="email">
+                  Email
+                  <input
+                    placeholder="Email"
+                    onChange={this.handleChange}
+                    className="input"
+                    name="email"
+                    value={email}
+                  />
+                </label>
+              </div>
+              <br></br>
+              <div>
+                <input className="submit-button" type="submit" />
+              </div>
+              <div>
+                <Link to="/account">
+                  <button className="cancel-button">Cancel</button>
+                </Link>
+              </div>
+            </div>
+          </form>
+        </div>
+      </div>
+    );
+  }
 }
 
 const mapState = (state) => {
   // console.log(state)
   return {
-    auth: state.auth
+    auth: state.auth,
   };
 };
 
-
-const mapDispatch = (dispatch) => (
-    {
-  updateUser: (auth) => dispatch(updateUser(auth))
-})
+const mapDispatch = (dispatch) => ({
+  updateUser: (auth) => dispatch(updateUser(auth)),
+});
 
 export default connect(mapState, mapDispatch)(EditAccount);
