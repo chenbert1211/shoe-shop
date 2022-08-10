beebd2df676f4a14b18ca41df43dd30ff542be54
Cole Berman <11coleandrew4@gmail.com>
2022-08-09 19:34:16 -0500

    most of the way done with checkout, just need to add button functionality

--- client/components/Checkout.js
+++ client/components/Checkout.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-
+// import { getUserCart } from '../store/redux/cart';
+import { updateUser } from '../store/auth';
 import { connect } from 'react-redux';
 
 class Checkout extends Component {
@@ -7,32 +8,112 @@ class Checkout extends Component {
     super(props);
   }
 
+  async componentDidMount() {
+    if (this.props.auth.id) {
+      // await this.props.getUserCart(this.props.auth.id);
+      await this.props.updateUser({
+        id: this.props.auth.id,
+        cart: this.props.Cart,
+      });
+    }
+  }
+
   render() {
+    const cartArray = this.props.auth.cart;
+    const user = this.props.auth;
+    const cardNumArr =
+      typeof user.creditCard === 'string' ? user.creditCard.split('') : '';
+
+    const lastFourNums = `${cardNumArr[15]}${cardNumArr[16]}${cardNumArr[17]}${cardNumArr[18]} `;
+    console.log('user', user);
     return (
       <div id="checkout-outmost">
-        <div id="checkout-container">
-          <div id="checkout">
-            <div id="product-name-checkout">Product Name</div>
-            <div>Product Quantity</div>
-            <div></div>
-            <div>Confirm Address</div>
-            <div>user address</div>
-            <button id="confirm-checkout-button" type="button">
-              Confirm and Checkout
-            </button>
+        <div id="checkout-items-outmost">
+          <div id="checkout-container">
+            <div id="checkout">
+              <div id="product-info-checkout">
+                <div className="checkout-payment-title">Items In Your Cart</div>
+                {cartArray?.map((cartItem, idx) => {
+                  if (idx !== cartArray.length - 1) {
+                    return (
+                      <div key={cartItem.id} id="checkout-item">
+                        <div id="checkout-item-label">Item {idx + 1}</div>
+                        <img
+                          src={cartItem.product.imageUrl}
+                          id="checkout-image"
+                        />
+                        <div>{cartItem.product.name}</div>
+                        <div>
+                          <span id="item-size-text">Size</span> {cartItem.size}-
+                          {cartItem.category}
+                        </div>
+                        <div id="checkout-item-seperator"></div>
+                      </div>
+                    );
+                  } else {
+                    return (
+                      <div key={cartItem.id} id="checkout-item">
+                        <div id="checkout-item-label">Item {idx + 1}</div>
+                        <img
+                          src={cartItem.product.imageUrl}
+                          id="checkout-image"
+                        />
+                        <div>{cartItem.product.name}</div>
+                        <div>
+                          <span id="item-size-text">Size</span> {cartItem.size}-
+                          {cartItem.category}
+                        </div>
+                      </div>
+                    );
+                  }
+                })}
+              </div>
+            </div>
           </div>
         </div>
+        <div id="checkout-user-info">
+          <div className="checkout-payment-title">
+            Payment & Shipping Information
+          </div>
+          <div className="payment-shipping-title">Shipping Address</div>
+          <div>
+            {user.address}, {user.city}, {user.state} {user.zipCode}
+          </div>
+          <div>
+            <div className="payment-shipping-title">Card Number</div>
+            ...............{lastFourNums}
+          </div>
+          <div className="payment-shipping-title">Card Expiration</div>
+          <div>{user.CardExp}</div>
+          <div className="payment-shipping-title">Name On Card</div>
+          <div>
+            {user.nameOnCard} {user.lastName}
+          </div>
+          <button id="confirm-checkout-button" type="button">
+            Confirm and Checkout
+          </button>
+        </div>
+        {/* <button id="confirm-checkout-button" type="button">
+          Confirm and Checkout
+        </button> */}
       </div>
-
-      // <div>
-      // <br/><br/><br/><br/><br/>
-      // This is checkout
-      // </div>
     );
   }
 }
 
-export default Checkout;
+const mapState = (state) => {
+  return {
+    Cart: state.cartReducer,
+    auth: state.auth,
+  };
+};
+
+const mapDispatch = (dispatch) => ({
+  // getUserCart: (id) => dispatch(getUserCart(id)),
+  updateUser: (auth) => dispatch(updateUser(auth)),
+});
+
+export default connect(mapState, mapDispatch)(Checkout);
 
 /*
 1. Import fetchSingleShoe
